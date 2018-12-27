d06f49eb290ca81b48c6a06a083c75a78a88d750
brainmtn <jf@cordon.dk>
2018-12-27 12:45:29 +0100

    Minor changes in main.js

--- script/main.js
+++ script/main.js
@@ -5,19 +5,17 @@ const imgs = document.querySelectorAll('.imgs img');
 
 imgs.forEach(imgs => imgs.addEventListener('click', imgClick));
 
+// Function for thumbnails on slideshow
 function imgClick(e) {
+
+    // Removes outline on selected image for slideshow
     imgs.forEach(img => (img.style.outline = ''));
-    imgs.forEach(img => (img.style.height = ''));
 
     //Changes current image to src of clicked image
     current.src = e.target.src;
     console.log(e.target.src);
     console.log(current.src);
 
-
     // Creates border around the selected thumbnail
     e.target.style.outline = "1px solid black";
-    e.target.style.height = "85px";
-
-
 }
